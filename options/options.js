6c386a4973f499da549c0399a1254dddb68bb37b
Crowdbotics <team@crowdbotics.com>
2021-11-10 10:18:52 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "smartwatertank_31908",
+  url: "https://smartwatertank_31908.botics.co",
+  api: "https://smartwatertank_31908.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
